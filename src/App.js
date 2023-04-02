1025cc97ee353b0a258f741b6d437bb13b5a65ca
Sagi <sagi403@gmail.com>
2023-04-02 11:54:40 +0300

    Create two dimensional array with random colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <h1>Initial Test</h1>;
+const App = () => {
+  const color = randomColor();
+  // let avatar = new Array(16)
+
+  let avatar = Array.from({ length: 16 }, () =>
+    Array.from({ length: 16 }).map(() => randomColor())
+  );
+  console.log(avatar);
+
+  return <h1 style={{ color }}>{avatar}</h1>;
+};
+
+function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 export default App;
